8542361335dc6916fca32f5aff681711dd550c18
EvgeniyLevkovich <jt.horus@gmail.com>
2020-06-24 22:35:25 +0300

    edited config.yml

--- src/test/java/tests/WorkspaceTest.java
+++ src/test/java/tests/WorkspaceTest.java
@@ -2,18 +2,22 @@ package tests;
 
 import org.testng.annotations.Test;
 
-public class WorkspaceTest extends LoginTest {
+public class WorkspaceTest extends BaseTest {
 
     @Test
     public void inviteUser() {
-        login();
+        loginSteps.signIn("test42@mailinator.com");
+        loginSteps.MailHogValidation("test42@mailinator.com");
+        loginSteps.selectWorkspace("Test Workspace");
         workspaceSteps.inviteUsersByEnterEmail("welcomenewuser@mailinator.com");
         workspaceSteps.checkForInvitedUser("welcomenewuser@mailinator.com");
     }
 
     @Test
     public void createNewFolder(){
-        login();
+        loginSteps.signIn("test42@mailinator.com");
+        loginSteps.MailHogValidation("test42@mailinator.com");
+        loginSteps.selectWorkspace("Test Workspace");
         workspaceSteps.createANewFolder();
         workspaceSteps.checkNewFolder();
     }
