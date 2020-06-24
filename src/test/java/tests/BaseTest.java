4ea17e15c01cdb809e8881c56c0755994a11725f
Astashonak <e.anischyk@gmail.com>
2020-06-24 19:52:29 +0300

    try resolve conflict2

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -3,21 +3,27 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+
 import pages.MailHogPage;
 import steps.LoginSteps;
 import steps.WorkspaceSteps;
 import utils.EmailGenerator;
 import utils.FolderGenerator;
-
+import pages.LoginPage;
+import steps.ChannelSteps;
+import utils.TextGenerator;
 
 public class BaseTest {
     LoginSteps loginSteps;
+    LoginTest loginTest;
+    LoginPage loginPage;
     WorkspaceSteps workspaceSteps;
+    ChannelTest channelTest;
+    ChannelSteps channelSteps;
     EmailGenerator emailGenerator;
     FolderGenerator folderGenerator;
     TextGenerator textGenerator;
     MailHogPage mailHogPage;
-
 
     @BeforeSuite
     public void setupBrowser() {
@@ -26,12 +32,13 @@ public class BaseTest {
         Configuration.startMaximized = true;
         Configuration.headless = false;
         Configuration.holdBrowserOpen = true;
-
     }
 
     @BeforeMethod
     public void openBrowser() {
         loginSteps = new LoginSteps();
+        loginTest = new LoginTest();
+        loginPage = new LoginPage();
         emailGenerator = new EmailGenerator();
         workspaceSteps = new WorkspaceSteps();
         folderGenerator = new FolderGenerator();
@@ -40,5 +47,4 @@ public class BaseTest {
         textGenerator = new TextGenerator();
         mailHogPage = new MailHogPage();
     }
-
 }
